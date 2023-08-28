a282d78939fd7a62cf1a37b2d43dbf2cb2a04372
Naeem Hossen <132334814+NHossen@users.noreply.github.com>
2023-08-29 02:09:17 +0400

    button added

--- script.js
+++ script.js
@@ -11,8 +11,18 @@ displayFullData(postData)//Call API
 
 const displayFullData=(postData)=>{ //#2 Get Data From Load data
 
-    //console.log(postData);
+    console.log(postData);
     const postContainer=document.getElementById('blog-data-container')//2.2 //Now need to show post in blog post card container
+
+
+    const showAllContainer=document.getElementById('show-all-container');
+   if(postData.length>4){
+       showAllContainer.classList.remove('hidden')
+   }else{
+    showAllContainer.classList.add('hidden')
+   } 
+    //console.log(postData.length);
+    postData=postData.slice(0,6)//Display data main page 6
     
     postData.forEach(data=>{//2.1 data show one by oner to user
         console.log(data);
@@ -26,11 +36,25 @@ const displayFullData=(postData)=>{ //#2 Get Data From Load data
         <img src="${data.image}" alt="Shoes" class="rounded-xl" />
       </figure>
       <div class="card-body items-center text-center">
-        <h2 class="card-title">${data.name}</h2>
-        <p>${data.description}</p>
-        <div class="card-actions">
-          <button class="btn btn-primary">Buy Now</button>
+        <h2 class="card-title ">Features</h2>
+
+        <ol class="text-left ">
+        ${data.features.map((feature,index) => `<li class="mb-2">${index +1}.${feature}</li>`).join('')}
+        </ol>
+
+        <div class="border-b-2 border-[#c2c2c2] w-[350px]">
+        
         </div>
+        <h2 class="card-title">${data.name}</h2>
+        <div class="flex gap-20 items-center ">
+        <p class="">Publised Date: ${data.published_in
+        }</p>
+        <a href="#">
+        <svg class="bg-[#ffdfdf] p-2 w-12 h-12 rounded-full" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+  <path d="M4.5 12H19.5M19.5 12L12.75 5.25M19.5 12L12.75 18.75" stroke="#EB5757" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg></a>
+        </div>
+        
       </div>
 
         `;
